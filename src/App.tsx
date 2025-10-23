d90540a0c25880dc7dc7786719241eb9ec5f8a53
Agostnho F  Teles Jr. <atelesjr@gmail.com>
2025-10-23 17:56:41 -0300

    styles(main page): making the main page layout and tailwind themes settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
 	return (
-		<div className="container">
-			<header className="app-header">
-				<h1>Art Explorer</h1>
-				<nav>
-					<div>Main</div>
-					<div>Your Favorites</div>
-				</nav>
-				<div className="search-bar">
-					<input type="text" placeholder="Search artworks..." />
-					<button>Search</button>
+		<div className="container-main">
+			<header className="header">
+				<div className="font-bold flex  flex-col items-center">
+					<div className="text-[45px] leading-none">ART</div>
+					<div className="text-[20px]">Explorer</div>
+				</div>
+
+				<div
+					className={`hamburger sm:hidden ${isMenuOpen ? 'active' : ''}`}
+					onClick={toggleMenu}
+				>
+					<div className="bar" />
+					<div className="bar" />
+					<div className="bar" />
+				</div>
+
+				<div className="hidden sm:flex">
+					<nav className="flex">
+						<div>Main</div>''
+						<div>Your Favorites</div>
+					</nav>
+					<div className="search-bar">
+						<button>Search</button>
+					</div>
 				</div>
 			</header>
 			<main>
