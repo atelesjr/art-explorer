4caa619d008d748cbd2941014fc5d21baa643e70
Agostnho F  Teles Jr. <atelesjr@gmail.com>
2025-11-23 18:15:25 -0300

    test: polyfill TextEncoder/TextDecoder in Jest setup (use globalThis)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+// Polyfill TextEncoder/TextDecoder for Jest environment (some libs expect these globals)
+// Use `globalThis` and a runtime `require` with ts-ignore to avoid TypeScript/typing issues
+if (typeof (globalThis as any).TextEncoder === 'undefined') {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	// @ts-ignore: require may not be typed in this TS configuration
+	const util = require('util');
+	(globalThis as any).TextEncoder = util.TextEncoder;
+	(globalThis as any).TextDecoder = util.TextDecoder;
+}
